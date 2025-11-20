b4bb24c85bade0dc1882ab29f9bf295d303d779d
duddysam <duddysam91@gmail.com>
2025-11-20 11:08:45 -0500

    implement command line functionality

--- potluck-app/app.js
+++ potluck-app/app.js
@@ -1,11 +1,16 @@
 const { client, connectDB } = require('./index')
+const yargs = require('yargs')
+const appUser = require('./appuser.js')
 
+yargs.version('1.1.0')
+
+// function for verification purposes only
 async function runApp(){
     await connectDB();
 
     try {
         const res = await client.query('SELECT * FROM appuser')
-        console.log(res)
+        console.log(res['rows'])
     } catch (err){
         console.error('Error executing query', err);
     } finally {
@@ -14,4 +19,63 @@ async function runApp(){
     }
 }
 
-runApp();
+// add a new user
+yargs.command({
+    command: 'add_user',
+    describe: 'adding a new potluck app user',
+    builder: {
+        f_name: {
+            describe: 'first name',
+            demandOption: true,
+            type: 'string'
+        },
+        l_name: {
+            describe: 'last name',
+            demandOption: true,
+            type: 'string'
+        },
+        user_name:{
+            describe: 'unique username',
+            demandOption: true,
+            type: 'string'
+        },
+        password:{
+            describe: 'password',
+            demandOption: true,
+            type: 'string'
+        },
+        email: {
+            describe: 'email',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: (argv) => {
+        appUser.addUser(argv.f_name, argv.l_name, argv.user_name, argv.password, argv.email)
+    }
+})
+
+// delete a user
+yargs.command({
+    command: 'delete_user',
+    describe: 'deleting user by username',
+    builder:{
+        username: {
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: (argv) => {
+        appUser.deleteUser(argv.username)
+    }
+})
+
+yargs.command({
+    command: 'get_users',
+    describe: 'see all users',
+    handler: (argv) => {
+        appUser.getUsers()
+    }
+})
+
+yargs.parse()
---
+++ potluck-app/appuser.js
@@ -0,0 +1,45 @@
+const validator = require('validator')
+const { client, connectDB } = require('./index')
+
+async function addUser(f_name, l_name, user_name, password, email) {
+    await connectDB();
+
+    try {
+        const res = await client.query(`INSERT INTO appuser VALUES ('${f_name}', '${l_name}', default, '${user_name}', '${password}', '${email}')`)
+    } catch (err) {
+        console.error('Error Executing query', err);
+    } finally {
+        await client.end();
+    }
+}
+
+
+async function deleteUser(username) {
+    await connectDB();
+
+    try {
+        const res = await client.query(`DELETE FROM appuser WHERE username = '${username}'`)
+    } catch (err) {
+        console.error('Error Executing Query', err);
+    } finally {
+        await client.end()
+    }
+}
+
+async function getUsers() {
+    await connectDB()
+    try {
+        const res = await client.query('SELECT * FROM appuser')
+        console.log(res['rows'])
+    } catch (err) {
+        console.error('Error Executing Query', err);
+    } finally {
+        await client.end()
+    }
+}
+
+module.exports = {
+    addUser: addUser,
+    deleteUser: deleteUser,
+    getUsers: getUsers
+}
